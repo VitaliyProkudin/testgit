f6f30fb6075b600aa12b8888578621c4694b6d83
VitaliyProkudin <vitaliy.prokudin@gmail.com>
2019-03-18 17:39:06 +0300

    Complite all

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-print("Hello world")
-print("everebody hello")
-print("everebody hello")
-print("everebody hello")
+import os
+import psutil
+
+print("Hello Student PY")
+print('Укажи какую операцию выполнить')
+print("1 - Тек. директории\n 2 - ") 
+answer = int(input())
+
+if answer == 1:
+    print(os.listdir())
+elif answer == 2:
+    print(psutil.cpu_times())
+elif answer == 3:
+    pass
+elif condition:
+    pass                
+else:
+    print("Введено некорректное число")
--- test1.py
+++
@@ -1,3 +0,0 @@
-a = input('Enter int:')
-for x in range(len(a)):
-    print(int(a[x]))
